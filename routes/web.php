f4790949f7da34dfae0131c0ce1d65ad03fa2697
Phquangson <Phquangson2003@gmail.com>
2025-07-02 22:07:46 +0700

    [Route][web.php]: Update Route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,11 @@
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
+Route::group(['prefix' => '/'], function () {
+    Route::get('/', [HomeController::class, 'index']);
+    Route::get('/index', [HomeController::class, 'index']);
+});
+
 Route::group(['prefix' => 'home'], function () {
     Route::get('/index', [HomeController::class, 'index']);
 });
