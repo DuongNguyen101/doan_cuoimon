93240d1bd2522b29980308af390c82e9cab3d702
Phquangson <Phquangson2003@gmail.com>
2025-07-07 11:25:41 +0700

    [Project]: Update About

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Categories;
+use App\Models\About; 
 
 class AboutController extends Controller
 {
-    public function index(){
-    $categories = Categories::all();
-    return view('template/user/about/index', compact('categories'));
-}
+    public function index()
+    {
+        $categories = Categories::all();
+        $abouts = About::all(); 
+
+        return view('template/user/about/index', compact('categories', 'abouts'));
+    }
 }
