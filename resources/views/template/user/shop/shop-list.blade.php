4bb49e663849326b18b9afd374bb23c80c71e3e8
Phquangson <Phquangson2003@gmail.com>
2025-07-14 01:52:42 +0700

    [Project]: Update layout user

--- resources/views/template/user/shop/shop-list.blade.php
+++ resources/views/template/user/shop/shop-list.blade.php
@@ -412,12 +412,12 @@
 
                     </div>
                     <div class="hr-line"></div>
-                    <div class="footer-wrapper pt-40 mb-32">
+                    <div class="footer-wrapper pt-40 mb-32 justify-content-center
+                    ">
                         <div class="store-desc">
-                            <a href="{{asset('template/user/home/index')}}"><img src="{{url('user')}}/media/logo.png" alt="" class="mb-16"></a>
+                            <a href="{{asset('template/user/home/index')}}"><img src="{{url('image/homepage/logo.png')}}" alt="" width="284px" height="43px"></a>
                             <p class="light-gray mb-xl-32 mb-lg-0 mb-32">
-                                Lorem ipsum dolor sit amet consectetur. Eu dolor faucibus <br>
-                                sit fames elit ac. Hendrerit ultrices morbi faucibus.
+                                Northwest specialties bring the bold flavors of the mountains: smoked buffalo meat, mak khen pepper, cham cheo dipping sauce... Simple yet rich in highland cultural identity.
                             </p>
                             <p class="d-xl-none d-lg-block d-none light-gray mb-32">
                                 amet consectetur. Eu dolor faucibus <br>
@@ -436,36 +436,25 @@
                             </a>
                         </div>
                         <div class="footer-link-block">
-                            <h6 class="fw-600 mb-24">Find It Fast</h6>
+                            <h6 class="fw-600 mb-24">All Categories</h6>
                             <ul class="list-unstyled m-0">
-                                <li class="mb-8"><a href="" class="light-gray">Laptop & Computer</a></li>
-                                <li class="mb-8"><a href="" class="light-gray">Camera & Photo</a></li>
-                                <li class="mb-8"><a href="" class="light-gray">Smartphone & Tablet</a></li>
-                                <li class="mb-8"><a href="" class="light-gray">TV & Audio</a></li>
-                                <li class="mb-8"><a href="" class="light-gray">Home & Electronic</a></li>
-                                <li class="mb-8"><a href="" class="light-gray">Headphone & Speakers</a></li>
+                                @foreach($categories as $category)
+                                    <li class="mb-8">
+                                        <a href="{{ url('/template/user/shop/category/' . $category->	category_id  . $category->slug) }}" class="light-gray">{{ $category->name }}</a>
+                                    </li>
+                                @endforeach
                             </ul>
                         </div>
                         <div class="footer-link-block">
                             <h6 class="fw-600 mb-24">Quick Link</h6>
                             <ul class="list-unstyled m-0">
                                 <li class="mb-8"><a href="{{asset('template/user/home/index')}}" class="light-gray">Home Page</a></li>
-                                <li class="mb-8"><a href="about.html" class="light-gray">About Us</a></li>
-                                <li class="mb-8"><a href="{{asset('template/user/shop/shop-list')}}" class="light-gray">Shop Now</a></li>
-                                <li class="mb-8"><a href="register.html" class="light-gray">Signup</a></li>
-                                <li class="mb-8"><a href="register.html" class="light-gray">Login</a></li>
-                                <li class="mb-8"><a href="contact.html" class="light-gray">Privacy Policy</a></li>
-                            </ul>
-                        </div>
-                        <div class="footer-link-block">
-                            <h6 class="fw-600 mb-24">Customer Care</h6>
-                            <ul class="list-unstyled m-0">
-                                <li class="mb-8"><a href="register.html" class="light-gray">My Account</a></li>
-                                <li class="mb-8"><a href="tracking.html" class="light-gray">Track Your Order</a></li>
-                                <li class="mb-8"><a href="shop-grid-3.html" class="light-gray">Customer Service</a></li>
-                                <li class="mb-8"><a href="blog-grid.html" class="light-gray">Blogs</a></li>
-                                <li class="mb-8"><a href="about.html" class="light-gray">FAQs</a></li>
-                                <li class="mb-8"><a href="coming-soon.html" class="light-gray">Coming Soon</a></li>
+                                <li class="mb-8"><a href="{{asset('template/user/shop/category/1')}}" class="light-gray">Shop Now</a></li>
+                                <li class="mb-8"><a href="{{asset('/template/user/blog/blog-grid')}}" class="light-gray">Blogs</a></li>
+                                <li class="mb-8"><a href="{{asset('/template/user/about/index')}}" class="light-gray">About Us</a></li>
+                                <li class="mb-8"><a href="{{asset('/template/user/pages/login')}}" class="light-gray">login</a></li>
+                                <li class="mb-8"><a href="{{asset('/template/user/pages/register')}}" class="light-gray">Register</a></li>
+                                <li class="mb-8"><a href="{{asset('/template/user/contact/index')}}" class="light-gray">Contact Us</a></li>
                             </ul>
                         </div>
                         <div class="footer-link-block">
@@ -561,19 +550,7 @@
                         <div class="payment-cards d-flex align-items-center gap-16">
                             <p class="fw-500 light-gray d-sm-flex d-none">Payment Mathods:</p>
                             <div class="card-block">
-                                <img src="{{url('user')}}/media/icons/card-1.png" alt="">
-                            </div>
-                            <div class="card-block">
-                                <img src="{{url('user')}}/media/icons/card-2.png" alt="">
-                            </div>
-                            <div class="card-block">
-                                <img src="{{url('user')}}/media/icons/card-3.png" alt="">
-                            </div>
-                            <div class="card-block">
-                                <img src="{{url('user')}}/media/icons/card-4.png" alt="">
-                            </div>
-                            <div class="card-block">
-                                <img src="{{url('user')}}/media/icons/card-5.png" alt="">
+                                <img src="{{url('image/checkout/vnp.png')}}" alt="" width="48px" height="33px">
                             </div>
                         </div>
                     </div>
